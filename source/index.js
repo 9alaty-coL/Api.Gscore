a55b52c364fd9af59598b3b7c7fd58d16f7d211d
9alaty-coL <trantanloc@gmail.com>
2022-03-14 22:41:45 +0700

    enable CORS

--- source/index.js
+++ source/index.js
@@ -1,18 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//  Enable CORS on Server
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 //router
-const route = require('./routes/index')
-route(app)
+const route = require("./routes/index");
+route(app);
 
 //database
-const db = require('./config/db/db')
-db()
+const db = require("./config/db/db");
+db();
 
-app.listen(port, ()=>{
-    console.log('App listening at port', port)
-})
+app.listen(port, () => {
+  console.log("App listening at port", port);
+});
